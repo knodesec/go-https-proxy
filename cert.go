ac29ffb37b2d9544578b540617fd6ed5abdf6c7a
knodesec <50653342+knodesec@users.noreply.github.com>
2021-01-25 11:24:53 +0000

    Create cert.go

---
+++ cert.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"crypto/tls"
+	"crypto/x509"
+
+	"github.com/elazarl/goproxy"
+)
+
+// Replace with cert and key from burp certificate
+var caKey = []byte(``)
+var caCert = []byte(``)
+
+func setCA(caCert, caKey []byte) error {
+	goproxyCa, err := tls.X509KeyPair(caCert, caKey)
+	if err != nil {
+		return err
+	}
+	if goproxyCa.Leaf, err = x509.ParseCertificate(goproxyCa.Certificate[0]); err != nil {
+		return err
+	}
+	goproxy.GoproxyCa = goproxyCa
+	goproxy.OkConnect = &goproxy.ConnectAction{Action: goproxy.ConnectAccept, TLSConfig: goproxy.TLSConfigFromCA(&goproxyCa)}
+	goproxy.MitmConnect = &goproxy.ConnectAction{Action: goproxy.ConnectMitm, TLSConfig: goproxy.TLSConfigFromCA(&goproxyCa)}
+	goproxy.HTTPMitmConnect = &goproxy.ConnectAction{Action: goproxy.ConnectHTTPMitm, TLSConfig: goproxy.TLSConfigFromCA(&goproxyCa)}
+	goproxy.RejectConnect = &goproxy.ConnectAction{Action: goproxy.ConnectReject, TLSConfig: goproxy.TLSConfigFromCA(&goproxyCa)}
+	return nil
+}
